9d40e5bc1319b801022eb45099824dcaf6a215d6
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-07-18 14:35:04 +0200

    Add one-million-a test to CRC32C

--- tests/crc32c/mod.rs
+++ tests/crc32c/mod.rs
@@ -4,12 +4,12 @@ use crypto_tests::hash::{Test, main_test, one_million_a};
 
 #[test]
 fn crc32c_main() {
-    let tests = new_tests!("1", "2", "3", "4");
+    let tests = new_tests!("1", "2", "3", "4", "5", "6");
     main_test::<pruefung::crc32c::CRC32C>(&tests);
 }
 
-// #[test]
-// fn crc32c_1million_a() {
-//     let output = include_bytes!("data/one_million_a.output.bin");
-//     one_million_a::<pruefung::crc32c::CRC32C>(output);
-// }
+#[test]
+fn crc32c_1million_a() {
+    let output = include_bytes!("data/one_million_a.output.bin");
+    one_million_a::<pruefung::crc32c::CRC32C>(output);
+}
