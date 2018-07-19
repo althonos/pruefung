0c013847cc1ab56b80009705a3922f09a4b300a8
Ferdia McKeogh <ferdia@mckeogh.tech>
2018-07-19 17:43:19 +0100

    Add crc16 and crc8

---
+++ src/crc/crc16/mod.rs
@@ -0,0 +1,3 @@
+pub mod crc16;
+
+pub use self::crc16::Crc16;
--- tests/crc/crc.rs
+++ tests/crc/crc.rs
@@ -40,3 +40,44 @@ mod crc32c {
     }
 
 }
+
+mod crc16 {
+
+    extern crate pruefung;
+
+    use crypto_tests;
+    use crypto_tests::hash::Test;
+
+    #[test]
+    fn main() {
+        let tests = new_tests!("crc16/1", "crc16/2", "crc16/3", "crc16/4", "crc16/5", "crc16/6");
+        crypto_tests::hash::main_test::<pruefung::crc::crc16::Crc16>(&tests);
+    }
+
+    #[test]
+    fn one_million_a() {
+        let output = include_bytes!("data/crc16/one_million_a.output.bin");
+        crypto_tests::hash::one_million_a::<pruefung::crc::crc16::Crc16>(output);
+    }
+
+}
+
+mod crc8 {
+
+    extern crate pruefung;
+
+    use crypto_tests;
+    use crypto_tests::hash::Test;
+
+    #[test]
+    fn main() {
+        let tests = new_tests!("crc8/1", "crc8/2", "crc8/3", "crc8/4", "crc8/5", "crc8/6");
+        crypto_tests::hash::main_test::<pruefung::crc::crc8::Crc8>(&tests);
+    }
+
+    #[test]
+    fn one_million_a() {
+        let output = include_bytes!("data/crc8/one_million_a.output.bin");
+        crypto_tests::hash::one_million_a::<pruefung::crc::crc8::Crc8>(output);
+    }
+}
